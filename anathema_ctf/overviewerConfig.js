c5e90eddddc459b96355c69fab4e2c803e11f7e6
mitchts <MCResourcePile@gmail.com>
2021-05-23 05:11:12 +0000

    Deploy to GitHub Pages - 521f6c43da576d1e0658983c929ee147af333c16

---
+++ anathema_ctf/overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "world"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1621738266",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Overviewer Render (smooth_lighting)",
+            "zoomLevels": 5,
+            "defaultZoom": 1,
+            "maxZoom": 5,
+            "path": "world-smooth_lighting",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world",
+            "last_rendertime": 1425232059,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -2,
+                9,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -2,
+                9,
+                0
+            ],
+            "north_direction": 0
+        }
+    ]
+};
