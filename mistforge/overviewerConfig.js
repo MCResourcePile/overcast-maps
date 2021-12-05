9170108afa5fccae017ab8dc9b5a4bd0073e7670
mitchts <MCResourcePile@gmail.com>
2021-12-05 13:37:13 +0000

    Deploy to GitHub Pages - d868365480d0e505046dc5cf5e1082a895fa7339

--- mistforge/overviewerConfig.js
+++ mistforge/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1621762552",
+        "cacheTag": "1638700538",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
